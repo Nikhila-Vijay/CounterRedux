3d489084e1f0a234ca1a5ec384b73e1bbfaa404b
Nikhila Vijay <nk2023vijay@gmail.com>
2024-08-23 00:21:02 +0530

    counter

---
+++ src/redux/store.js
@@ -0,0 +1,11 @@
+
+import {configureStore} from "@reduxjs/toolkit"
+import counterSlice from "./counterSlice"
+
+
+//we have to use a method configurestore to initialize this file as store
+export const store = configureStore({
+    reducer:{
+        counter:counterSlice
+    }
+})
